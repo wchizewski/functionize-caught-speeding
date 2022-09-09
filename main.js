a2061c7e3e43b8deb3458836750cbcc8c2f39962
wchizewski <90284175+wchizewski@users.noreply.github.com>
2022-09-09 14:23:17 -0600

    functionsasize

--- main.js
+++ main.js
@@ -13,42 +13,21 @@ function btnClicked() {
   let d3Limit = +document.getElementById('d3-limit').value;
 
   // Calculate & Output Ticket Types
-  // Driver 1
-  let d1Ticket;
-  if (d1Speed > d1Limit + 40) {
-    d1Ticket = 'Really Big Ticket';
-  } else if (d1Speed > d1Limit + 20) {
-    d1Ticket = 'Big Ticket';
-  } else if (d1Speed > d1Limit) {
-    d1Ticket = 'Small Ticket';
-  } else {
-    d1Ticket = 'No Ticket';
-  }
-  document.getElementById('d1-ticket').innerHTML = d1Ticket;
+  document.getElementById('d1-ticket').innerHTML = calcTicket(d1Speed, d1Limit);
+  document.getElementById('d2-ticket').innerHTML = calcTicket(d2Speed, d2Limit);
+  document.getElementById('d3-ticket').innerHTML = calcTicket(d3Speed, d3Limit);
+}
 
-  // Driver 2
-  let d2Ticket;
-  if (d2Speed > d2Limit + 40) {
-    d2Ticket = 'Really Big Ticket';
-  } else if (d2Speed > d2Limit + 20) {
-    d2Ticket = 'Big Ticket';
-  } else if (d2Speed > d2Limit) {
-    d2Ticket = 'Small Ticket';
+function calcTicket(speed, limit) {
+  let ticket;
+  if (speed > limit + 40) {
+    ticket = 'Really Big Ticket';
+  } else if (speed > limit + 20) {
+    ticket = 'Big Ticket';
+  } else if (speed > limit) {
+    ticket = 'Small Ticket';
   } else {
-    d2Ticket = 'No Ticket';
+    ticket = 'No Ticket';
   }
-  document.getElementById('d2-ticket').innerHTML = d2Ticket;
-
-  // Driver 3
-  let d3Ticket;
-  if (d3Speed > d3Limit + 40) {
-    d3Ticket = 'Really Big Ticket';
-  } else if (d3Speed > d3Limit + 20) {
-    d3Ticket = 'Big Ticket';
-  } else if (d3Speed > d3Limit) {
-    d3Ticket = 'Small Ticket';
-  } else {
-    d3Ticket = 'No Ticket';
-  }
-  document.getElementById('d3-ticket').innerHTML = d3Ticket;
+  return ticket;
 }
